b7557f64ee1a95cba04480d27959b5915451b1b1
Mithu <142711288+Mithu3311@users.noreply.github.com>
2024-12-10 18:00:53 +0500

    added add to cart in icon

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -131,19 +131,61 @@ const Navbar = () => {
           <div className="flex md:hidden gap-4 mt-3">
           <Link href={"/components/myAccountPage"}>
           <Button variant={"link"}>
-            <Image width={20} height={20} src={mdiAccount} alt="Account" />
+            <Image width={24} height={24} src={mdiAccount} alt="Account" />
           </Button>
         </Link>
         <Link href={"#"}>
           <Button variant={"link"}>
-            <Image width={20} height={20} src={search} alt="Search" />
+            <Image width={24} height={24} src={search} alt="Search" />
           </Button>
         </Link>
         <Link href={"#"}>
           <Button variant={"link"}>
-            <Image width={20} height={20} src={heart} alt="Favorites" />
+            <Image width={24} height={24} src={heart} alt="Favorites" />
           </Button>
         </Link>
+
+        {/* Add to Cart Sidebar */}
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant={"link"}>
+              <Image width={24} height={24} src={cart} alt="Cart" />
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="right" className="w-[300px] bg-gray-50">
+            <SheetHeader className="mt-5">
+              <SheetTitle className="text-xl font-semibold">
+              Shopping Cart
+              <hr className="mt-6"/>
+              </SheetTitle>
+            </SheetHeader>
+            <div className="mt-6">
+              <div className="flex justify-between flex-col w-full">
+                <div className="flex justify-start items-center gap-3 shadow-lg rounded-xl h-[105px] w-[350] mb-52">
+                  <Image src={sofaChair} height={105} width={108} alt="Picture"/>
+                  <div className="flex flex-col justify-start items-start gap-1">
+                    <h1 className="text-[16px] font-normal">Asgaard sofa</h1>
+                    <div className="flex gap-2">
+                      <p className="text-[12px] text-gray-600">1</p>
+                      <p className="text-[12px] text-gray-600">X</p>
+                      <p className="text-[12px] text-[#B88E2F]">Rs. 250,000.00</p>
+                    </div>
+                  </div>
+                  <CircleX className="fill-slate-700 text-white"/>
+                </div>
+                <div className="flex justify-between items-center w-full mb-3">
+                  <h1>Subtotal</h1>
+                  <h1>Rs. 250,000.00</h1>
+                </div>
+                <hr />
+                <div className="flex justify-between items-center">
+                    <Button variant={'outline'} className="text-[12px] rounded-full w-[120px] h-[31px]">View Cart</Button>
+                    <Button variant={'outline'} className="text-[12px] rounded-full w-[120px] h-[31px]">Checkout</Button>
+                </div>
+              </div>
+            </div>
+          </SheetContent>
+        </Sheet>
           </div>
         </SheetContent>
       </Sheet>
