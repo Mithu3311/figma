9ab6a31ad3dc864ae146b6e96675a733ce45fe4f
Mithu <142711288+Mithu3311@users.noreply.github.com>
2024-12-10 16:52:21 +0500

    updated add to cart button

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -7,21 +7,30 @@ import mdiAccount from "../../../public/mdiAccountAlert.svg";
 import search from "../../../public/search.svg";
 import heart from "../../../public/heartIcon.svg";
 import cart from "../../../public/shoppingCart.svg";
-import { Menu } from "lucide-react";
-import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
+import { CircleX, CrossIcon, Menu } from "lucide-react";
+import sofaChair from '../../../public/sofaChair.png'
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 
 const poppins = Poppins({ subsets: ["latin"], weight: ["400", "500", "700"] });
 
 const navLinks = [
   { href: "/", label: "Home" },
-  { href: "./components/shopPage", label: "Shop" },
+  { href: "/components/shopPage", label: "Shop" },
   { href: "#", label: "About" },
-  { href: "#", label: "Contact" },
+  { href: "/components/contactPage", label: "Contact" },
 ];
 
 const Navbar = () => {
   return (
-    <div className={`${poppins.className} w-full bg-Mybackgroundcolor flex p-4 justify-evenly items-center m-0`}>
+    <div
+      className={`${poppins.className} w-full bg-Mybackgroundcolor flex p-4 justify-evenly items-center m-0`}
+    >
       {/* Logo */}
       <h1 className="text-xl font-bold w-[247px]"></h1>
 
@@ -40,18 +49,63 @@ const Navbar = () => {
 
       {/* Icons */}
       <div className="hidden md:flex gap-4">
-        <Button variant={"link"}>
-          <Image width={24} height={24} src={mdiAccount} alt="Account" />
-        </Button>
-        <Button variant={"link"}>
-          <Image width={24} height={24} src={search} alt="Search" />
-        </Button>
-        <Button variant={"link"}>
-          <Image width={24} height={24} src={heart} alt="Favorites" />
-        </Button>
-        <Button variant={"link"}>
-          <Image width={24} height={24} src={cart} alt="Cart" />
-        </Button>
+        <Link href={"/components/myAccountPage"}>
+          <Button variant={"link"}>
+            <Image width={24} height={24} src={mdiAccount} alt="Account" />
+          </Button>
+        </Link>
+        <Link href={"#"}>
+          <Button variant={"link"}>
+            <Image width={24} height={24} src={search} alt="Search" />
+          </Button>
+        </Link>
+        <Link href={"#"}>
+          <Button variant={"link"}>
+            <Image width={24} height={24} src={heart} alt="Favorites" />
+          </Button>
+        </Link>
+
+        {/* Add to Cart Sidebar */}
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant={"link"}>
+              <Image width={24} height={24} src={cart} alt="Cart" />
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="right" className="w-[300px] bg-gray-50">
+            <SheetHeader className="mt-5">
+              <SheetTitle className="text-xl font-semibold">
+              Shopping Cart
+              <hr className="mt-6"/>
+              </SheetTitle>
+            </SheetHeader>
+            <div className="mt-6">
+              <div className="flex justify-between flex-col w-full">
+                <div className="flex justify-start items-center gap-3 shadow-lg rounded-xl h-[105px] w-[350] mb-52">
+                  <Image src={sofaChair} height={105} width={108} alt="Picture"/>
+                  <div className="flex flex-col justify-start items-start gap-1">
+                    <h1 className="text-[16px] font-normal">Asgaard sofa</h1>
+                    <div className="flex gap-2">
+                      <p className="text-[12px] text-gray-600">1</p>
+                      <p className="text-[12px] text-gray-600">X</p>
+                      <p className="text-[12px] text-[#B88E2F]">Rs. 250,000.00</p>
+                    </div>
+                  </div>
+                  <CircleX className="fill-slate-700 text-white"/>
+                </div>
+                <div className="flex justify-between items-center w-full mb-3">
+                  <h1>Subtotal</h1>
+                  <h1>Rs. 250,000.00</h1>
+                </div>
+                <hr />
+                <div className="flex justify-between items-center">
+                    <Button variant={'outline'} className="text-[12px] rounded-full w-[120px] h-[31px]">View Cart</Button>
+                    <Button variant={'outline'} className="text-[12px] rounded-full w-[120px] h-[31px]">Checkout</Button>
+                </div>
+              </div>
+            </div>
+          </SheetContent>
+        </Sheet>
       </div>
 
       {/* Responsive */}
