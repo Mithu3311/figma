9d3ac61f23842145bbdbd870a0ea0d75f430baa9
Mithu <142711288+Mithu3311@users.noreply.github.com>
2024-12-10 18:07:28 +0500

    Update navbar.tsx

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -8,7 +8,7 @@ import search from "../../../public/search.svg";
 import heart from "../../../public/heartIcon.svg";
 import cart from "../../../public/shoppingCart.svg";
 import { CircleX, Menu } from "lucide-react";
-import sofaChair from "../../../public/sofaChair.png";
+import sofaChair from '../../../public/sofaChair.png'
 import {
   Sheet,
   SheetContent,
@@ -29,10 +29,10 @@ const navLinks = [
 const Navbar = () => {
   return (
     <div
-      className={`${poppins.className} w-full bg-Mybackgroundcolor flex flex-wrap p-4 justify-between items-center`}
+      className={`${poppins.className} w-full bg-Mybackgroundcolor flex p-4 justify-evenly items-center m-0`}
     >
       {/* Logo */}
-      <h1 className="text-xl font-bold w-auto">Logo</h1>
+      <h1 className="text-xl font-bold w-[247px]"></h1>
 
       {/* Navigation Links */}
       <ul className="hidden md:flex gap-6">
@@ -48,7 +48,7 @@ const Navbar = () => {
       </ul>
 
       {/* Icons */}
-      <div className="hidden md:flex gap-4 items-center">
+      <div className="hidden md:flex gap-4">
         <Link href={"/components/myAccountPage"}>
           <Button variant={"link"}>
             <Image width={24} height={24} src={mdiAccount} alt="Account" />
@@ -72,51 +72,35 @@ const Navbar = () => {
               <Image width={24} height={24} src={cart} alt="Cart" />
             </Button>
           </SheetTrigger>
-          <SheetContent side="right" className="w-[300px] sm:w-full bg-gray-50">
+          <SheetContent side="right" className="w-[300px] bg-gray-50">
             <SheetHeader className="mt-5">
               <SheetTitle className="text-xl font-semibold">
-                Shopping Cart
-                <hr className="mt-6" />
+              Shopping Cart
+              <hr className="mt-6"/>
               </SheetTitle>
             </SheetHeader>
             <div className="mt-6">
-              <div className="flex flex-col gap-4">
-                <div className="flex items-center gap-3 shadow-lg rounded-xl p-3">
-                  <Image
-                    src={sofaChair}
-                    height={105}
-                    width={108}
-                    alt="Picture"
-                    className="w-[80px] h-[80px] object-cover"
-                  />
-                  <div className="flex flex-col gap-1">
+              <div className="flex justify-between flex-col w-full">
+                <div className="flex justify-start items-center gap-3 shadow-lg rounded-xl h-[105px] w-[350] mb-52">
+                  <Image src={sofaChair} height={105} width={108} alt="Picture"/>
+                  <div className="flex flex-col justify-start items-start gap-1">
                     <h1 className="text-[16px] font-normal">Asgaard sofa</h1>
-                    <div className="flex gap-2 items-center text-[12px] text-gray-600">
-                      <p>1</p>
-                      <p>X</p>
-                      <p className="text-[#B88E2F]">Rs. 250,000.00</p>
+                    <div className="flex gap-2">
+                      <p className="text-[12px] text-gray-600">1</p>
+                      <p className="text-[12px] text-gray-600">X</p>
+                      <p className="text-[12px] text-[#B88E2F]">Rs. 250,000.00</p>
                     </div>
                   </div>
-                  <CircleX className="text-gray-600 cursor-pointer" />
+                  <CircleX className="fill-slate-700 text-white"/>
                 </div>
-                <div className="flex justify-between items-center w-full">
+                <div className="flex justify-between items-center w-full mb-3">
                   <h1>Subtotal</h1>
                   <h1>Rs. 250,000.00</h1>
                 </div>
                 <hr />
-                <div className="flex justify-between gap-4">
-                  <Button
-                    variant={"outline"}
-                    className="text-[12px] rounded-full w-[120px] h-[31px]"
-                  >
-                    View Cart
-                  </Button>
-                  <Button
-                    variant={"outline"}
-                    className="text-[12px] rounded-full w-[120px] h-[31px]"
-                  >
-                    Checkout
-                  </Button>
+                <div className="flex justify-between items-center">
+                    <Button variant={'outline'} className="text-[12px] rounded-full w-[120px] h-[31px]">View Cart</Button>
+                    <Button variant={'outline'} className="text-[12px] rounded-full w-[120px] h-[31px]">Checkout</Button>
                 </div>
               </div>
             </div>
@@ -124,16 +108,16 @@ const Navbar = () => {
         </Sheet>
       </div>
 
-      {/* Responsive Menu */}
+      {/* Responsive */}
       <Sheet>
-        <SheetTrigger className="md:hidden">
+        <SheetTrigger className="flex md:hidden">
           <Menu />
         </SheetTrigger>
-        <SheetContent side="left" className="bg-white w-full">
+        <SheetContent className="bg-white">
           <SheetHeader>
-            <SheetTitle>Menu</SheetTitle>
+            <SheetTitle></SheetTitle>
           </SheetHeader>
-          <ul className="flex flex-col gap-4">
+          <ul className="flex flex-col md:hidden gap-6">
             {navLinks.map((link) => (
               <Link
                 key={link.label}
@@ -144,6 +128,65 @@ const Navbar = () => {
               </Link>
             ))}
           </ul>
+          <div className="flex md:hidden gap-4 mt-3">
+            <Link href={"/components/myAccountPage"}>
+          <Button variant={"link"}>
+            <Image width={24} height={24} src={mdiAccount} alt="Account" />
+          </Button>
+        </Link>
+        <Link href={"#"}>
+          <Button variant={"link"}>
+            <Image width={24} height={24} src={search} alt="Search" />
+          </Button>
+        </Link>
+        <Link href={"#"}>
+          <Button variant={"link"}>
+            <Image width={24} height={24} src={heart} alt="Favorites" />
+          </Button>
+        </Link>
+
+        {/* Add to Cart Sidebar */}
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant={"link"}>
+              <Image width={24} height={24} src={cart} alt="Cart" />
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="right" className="w-[300px] bg-gray-50">
+            <SheetHeader className="mt-5">
+              <SheetTitle className="text-xl font-semibold">
+              Shopping Cart
+              <hr className="mt-6"/>
+              </SheetTitle>
+            </SheetHeader>
+            <div className="mt-6">
+              <div className="flex justify-between flex-col w-full">
+                <div className="flex justify-start items-center gap-3 shadow-lg rounded-xl h-[105px] w-[350] mb-52">
+                  <Image src={sofaChair} height={105} width={108} alt="Picture"/>
+                  <div className="flex flex-col justify-start items-start gap-1">
+                    <h1 className="text-[16px] font-normal">Asgaard sofa</h1>
+                    <div className="flex gap-2">
+                      <p className="text-[12px] text-gray-600">1</p>
+                      <p className="text-[12px] text-gray-600">X</p>
+                      <p className="text-[12px] text-[#B88E2F]">Rs. 250,000.00</p>
+                    </div>
+                  </div>
+                  <CircleX className="fill-slate-700 text-white"/>
+                </div>
+                <div className="flex justify-between items-center w-full mb-3">
+                  <h1>Subtotal</h1>
+                  <h1>Rs. 250,000.00</h1>
+                </div>
+                <hr />
+                <div className="flex justify-between items-center">
+                    <Button variant={'outline'} className="text-[12px] rounded-full w-[120px] h-[31px]">View Cart</Button>
+                    <Button variant={'outline'} className="text-[12px] rounded-full w-[120px] h-[31px]">Checkout</Button>
+                </div>
+              </div>
+            </div>
+          </SheetContent>
+        </Sheet>
+          </div>
         </SheetContent>
       </Sheet>
     </div>
