abd7a86c88bcb24f7ef7b97fdf62d36d87d57465
Mithu <142711288+Mithu3311@users.noreply.github.com>
2025-01-25 16:11:01 +0500

    deploy

--- src/app/components/shop/productId.tsx
+++ src/app/components/shop/productId.tsx
@@ -1,8 +1,8 @@
 import { notFound } from "next/navigation";
 import { productById } from "@/sanity/lib/queries";
 import { sanityFetch } from "@/sanity/lib/fetch";
-import ProductDetails from "../../ProductDetails";
-import RelatedProducts from "../relatedProducts";
+import ProductDetails from "../ProductDetails";
+import RelatedProducts from "./relatedProducts";
 
 type Product = {
   _id: string;
